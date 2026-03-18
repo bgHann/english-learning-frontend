73b448efaf6501458db116a9ce876af16d967bea
Bui Gia Han <hanbuii7705@gmail.com>
2026-03-18 15:08:23 +0700

    update danh sánh khóa

--- src/components/course/CourseList.tsx
+++ src/components/course/CourseList.tsx
@@ -3,7 +3,7 @@ import { courses } from "@/data/courses";
 
 export default function CourseList() {
   return (
-    <div className="grid grid-cols-3 gap-6">
+    <div className="grid md:grid-cols-3 gap-6 gird-cols-1">
       {courses.map((course) => (
         <CourseCard key={course.id} course={course} />
       ))}
