55bbcfbefb9f2b4dea7fe4a8428d401df61c802c
Bui Gia Han <hanbuii7705@gmail.com>
2026-03-18 15:05:04 +0700

    update page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Header from "@/components/Header/page";
 import Image from "next/image";
+import Dashboard from "./learn/dashboard/page";
+import LangdingPage from "./Landing/page";
 
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <Header></Header>
+      <LangdingPage></LangdingPage>
     </div>
   );
 }
